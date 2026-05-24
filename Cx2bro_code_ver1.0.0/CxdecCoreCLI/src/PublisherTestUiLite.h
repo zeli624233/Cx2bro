03b0a1c63af29ea311db246826176aa8a656818e
ユイ可愛ね <2752630240@qq.com>
2026-05-25 02:47:19 +0800

    Delete Cx2bro_code_ver1.0.0 directory

--- Cx2bro_code_ver1.0.0/CxdecCoreCLI/src/PublisherTestUiLite.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include <string>
-
-int RunPublisherTestUi(const std::wstring& workspace,
-                       const std::wstring& extensionDirectory,
-                       const std::wstring& iconPath);
--- Cx2bro_code_ver1.0.0/CxdecCoreCLI/src/RestoreUiLite.h
+++
@@ -1,7 +0,0 @@
-#pragma once
-
-#include <string>
-
-int RunRestoreUi(const std::wstring& workspace,
-                 unsigned int workerCount,
-                 const std::wstring& iconPath);
